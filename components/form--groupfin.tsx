ed441aceef42a4688bd33ee7d9af0303a29597a4
septembre-io <contact@septembre.io>
2022-10-19 11:33:49 +0200

    upd groupfin

--- components/form--groupfin.tsx
+++ components/form--groupfin.tsx
@@ -119,7 +119,7 @@ export function FormGroupfin({ className, ...props }: FormArticleProps) {
             >
                 <option>1</option>
                 <option>cat 1</option>
-                <option>{t("29876849-c910-4ee3-8453-51dbe9d55bf2")}</option>
+                <option>29876849-c910-4ee3-8453-51dbe9d55bf2</option>
               </select>
               <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
