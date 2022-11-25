2ee8829f51a054034c155e454291cda04dfcb553
Arnaud RAMPONNEAU <18610272+ramponneau@users.noreply.github.com>
2022-11-25 18:59:34 +0100

    More details

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ import classNames from "classnames"
 export function Footer({ }: FooterProps) {
   return (
     <>
-    <footer className="bg-white container text-center content-center px-2 mx-auto">
+    <footer className="bg-white container text-gray-400 text-center content-center px-2 mx-auto">
 
 
 FEDERAGE SAS, SIREN n°828743369, Marque déposée à l’INPI, 2022.
--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ export default function IndexPage({ nodes, menus, blocks, }: IndexPageProps) {
 <div className="container pt-25 pb-25">
 
 
-        <h1 className="mb-3 text-3xl font-black text-center"><p>Tous acteurs de </p> <span class="fedbluetext">l'économie réelle.</span></h1>
+        <h1 className="mb-3 text-4xl font-black text-center"><p>Tous acteurs de </p> <span class="fedbluetext">l'économie réelle.</span></h1>
         <h2 className="mb-10 text-2xl text-gray-400 text-center">Solution de financement-capital pour entreprise</h2>
 
 </div>
