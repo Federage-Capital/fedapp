7e05107c6afdf6a4e4b02bb78eb6249c479d99ad
septembre-io <contact@septembre.io>
2022-12-09 15:22:01 +0100

    update search

--- pages/alluserlist.tsx
+++ pages/alluserlist.tsx
@@ -1,105 +1,258 @@
 import * as React from "react";
-import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
-import { DrupalNode } from "next-drupal";
 import Link from "next/link";
-import { DrupalJsonApiParams } from "drupal-jsonapi-params";
-import { getSession, useSession, signOut } from "next-auth/react";
+import Image from "next/image"
+import { DrupalNode,
+  getSearchIndexFromContext,
+  deserialize,
+  JsonApiSearchApiResponse,
+  DrupalSearchApiFacet, } from "next-drupal";
 import { useTranslation } from "next-i18next"
+import { GetStaticPropsResult } from "next"
 import { useRouter } from "next/router"
-
 import { getGlobalElements } from "lib/get-global-elements"
 import { Layout, LayoutProps } from "components/layout"
-import { drupal } from "lib/drupal";
-
-
-
 import { PageHeader } from "components/page-header"
-interface AlluserlistPageProps extends LayoutProps {
-  user: DrupalNode[];
- groups: DrupalNode[];
- grouprelatioshiptype: DrupalNode[];
+import { DrupalJsonApiParams } from "drupal-jsonapi-params";
+
+
+const params = {
+  fields: {
+    "user--user":
+      "name,type,filename,path,drupal_internal__uid,links",
+      "file--file":
+        "filename, uri",
+  },
+  filter: {},
+  include: "user_picture, user_picture.uid",
+
+
 }
 
-export default function AlluserlistPage({ menus, blocks, users, groups, members, greltypes }: AlluserlistPageProps) {
+
+
+export default function AlluserlistPage
+({ menus, blocks, users, nodes,
+  facets: initialFacets,
+}: AlluserlistPageProps) {
   const { t } = useTranslation()
   const router = useRouter()
-  const { status } = useSession()
+
+  const [status, setStatus] = React.useState<"error" | "success" | "loading">()
+  const [results, setResults] = React.useState<DrupalNode[]>(nodes)
+  const [facets, setFacets] =
+    React.useState<DrupalSearchApiFacet[]>(initialFacets)
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    for (const filter of ["fulltext", "name", "field_status"]) {
+      if (event.target[filter]?.value != "") {
+        params["filter"][filter] = event.target[filter]?.value
+      }
+    }
+
+
+    setStatus("loading")
+    const response = await fetch("/api/search/default_index", {
+      method: "POST",
+      body: JSON.stringify({
+        deserialize: false,
+        params,
+      }),
+    })
+
+    if (!response.ok) {
+      return setStatus("error")
+    }
+
+    setStatus("success")
+
+    const json = await response.json()
+    const results = deserialize(json) as DrupalNode[]
+
+    setResults(results)
+
+    if (results?.length) {
+      setFacets(json.meta.facets)
+    }
+  }
 
 
   return (
-    <Layout meta={{ title: t("All user list") }} menus={menus} blocks={blocks}>
+    <Layout meta={{ title: t("Explorer") }} menus={menus} blocks={blocks}>
       <PageHeader
-        heading={t("All user list")}
+        heading={t("Explorer")}
         breadcrumbs={[
           {
-            title: t("All user list"),
+            title: t("explorer"),
           },
         ]}
       />
 
-      <p>  group relatioship type : ????</p>
-      {greltypes.map((greltype) => (
-               <li key={greltypes.id}>
-                   <a className="text-sm font-semibold transition-colors hover:bg-black hover:underline">
-                <p>   {greltypes}</p>
-
-                   </a>
-               </li>
-             ))}
 
 
+        <form onSubmit={handleSubmit} className="mb-4">
+          <input
+            type="search"
+            placeholder="Search properties (min 4 characters)..."
+            name="fulltext"
+            className="relative block w-full col-span-5 px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-black focus:border-black focus:z-10 sm:text-sm"
+          />
+          <div className="grid gap-4 py-4 md:grid-cols-2">
+            {facets.map((facet) => (
+              <div key={facet.id} className="p-4 border rounded-md">
+                <h3 className="mt-0 mb-2 text-base">{facet.label}</h3>
+                <div className="grid grid-flow-row gap-2">
+                  <label
+                    htmlFor={`${facet.id}--any`}
+                    className="flex items-center text-base"
+                  >
+                    <input
+                      type="radio"
+                      id={`${facet.id}--any`}
+                      name={facet.path}
+                      className="mr-2"
+                      value=""
+                      defaultChecked
+                    />
+                    Any
+                  </label>
+                  {facet.terms.map((term) => (
+                    <label
+                      key={term.url}
+                      htmlFor={`${facet.id}--${term.values.value}`}
+                      className="flex items-center text-sm"
+                    >
+                      <input
+                        type="radio"
+                        id={`${facet.id}--${term.values.value}`}
+                        name={facet.path}
+                        value={term.values.value}
+                        className="mr-2"
+                        defaultChecked={term.values.active}
+                      />
+                      {term.values.label}{" "}
+                      <span className="flex items-center justify-center w-5 h-5 ml-2 text-xs bg-gray-200 rounded-full">
+                        {term.values.count}
+                      </span>
+                    </label>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex">
+            <button
+              type="button"
+              className="flex justify-center w-24 px-4 py-2 mr-4 text-sm font-medium text-gray-500 bg-white border border-gray-500 rounded-md shadow-sm hover:bg-gray-200"
+              onClick={() => {
+                router.reload()
+              }}
+            >
+              Reset
+            </button>
+            <button
+              type="submit"
+              data-cy="btn-submit"
+              className="flex justify-center w-48 px-4 py-2 text-sm font-medium text-white bg-black border border-transparent rounded-md shadow-sm hover:bg-black"
+            >
+              {status === "loading" ? "Please wait..." : "Search"}
+            </button>
+          </div>
+        </form>
+        {status === "error" ? (
+          <div className="px-4 py-2 text-sm text-red-600 bg-red-100 border-red-200 rounded-md">
+            An error occured. Please try again.
+          </div>
+        ) : null}
+        {!results.length ? (
+          <p className="text-sm" data-cy="search-no-results">
+            No results found.
+          </p>
+        ) : (
+          <div className="pt-4">
+            <div className="grid gap-6 md:grid-cols-2">
+              {results.map((node) => (
+                <div key={node.id}>
+                  <article
+                    className="grid grid-cols-3 gap-4"
+                    data-cy="search-result"
+                  >
 
 
 
+{node.field_images?.[0]?.field_media_image.uri && (
+  <div className="col-span-1 overflow-hidden rounded-md">
+    <Image
+      src={`${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}${node.field_images?.[0]?.field_media_image.uri.url}`}
+      width={200}
+      height={200}
+      layout="responsive"
+      objectFit="cover"
+    />
+  </div>
+)}
 
-             <p>  Tous les Projets </p>
-             <p>  Tous les Projets </p>
+                    <div className="col-span-2">
+                      <h4 className="mt-0 text-base">{node.name}</h4>
+                      <p className="m-0 text-base">For
 
-    <p>  Tous les Projets </p>
-    {groups.map((group) => (
-             <li key={group.id}>
-                 <a className="text-sm font-semibold transition-colors hover:bg-black hover:underline">
-              <p>   {group.label}</p>
-<p> {group.langcode}</p>
-<p>révision le  {group.revision_created}</p>
-<p>créé le   {group.created}</p>
-<p>Proprio  du projet {group.uid.display_name}</p>
-<Link href="1" passHref>
-  <a className="inline-flex items-center px-6 py-2 border border-gray-600 rounded-full hover:bg-gray-100">
-chemin : {group.path.alias}
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="w-4 h-4 ml-2"
-    >
-      <path d="M5 12h14M12 5l7 7-7 7" />
-    </svg>
-  </a>
-</Link>
-                 </a>
-             </li>
-           ))}
+                      {node.field_description?.value && (
+                        <div
+                          dangerouslySetInnerHTML={{ __html: node.field_description?.value }}
+                          className="mt-6 text-xl leading-loose prose"
+                        />
+                      )}
 
 
-<p>  Membres : toutes les participations dans tous les groupes</p>
-{members.map((member) => (
-         <li key={member.id}>
-             <a className="text-sm font-semibold transition-colors hover:bg-black hover:underline">
 
-            <p> Type de groupe  {member.type}</p>
-            <p> Id user dans le  groupe  {member.id}</p>
-              <p> Id user dans le  groupe Drupal Internat ID  {member.drupal_internal__id}</p>
-              <p>   {member.label}</p>
-<p> {member.path.alias}</p>
-<p> {member.revision_created}</p>
-<p> {member.uid.type}</p>
-             </a>
-         </li>
-       ))}
+                      </p>
+
+                      <p className="m-0 text-base">
+                      {node.field_site_internet?.uri && (
+                        <div
+                          dangerouslySetInnerHTML={{ __html: node.field_site_internet?.uri }}
+                          className="mt-6 text-xl leading-loose prose"
+                        />
+                      )}
+
+
+{node && (
+  <div
+    dangerouslySetInnerHTML={{ __html: node }}
+    className="mt-6 text-xl leading-loose prose"
+  />
+)}
+
+
+
+{node.user_picture?.id && (
+  <div
+    dangerouslySetInnerHTML={{ __html: node.user_picture?.id }}
+    className="mt-6 text-xl leading-loose prose"
+  />
+)}
+
+                      {node.user_picture?.id && (
+                        <div
+                          dangerouslySetInnerHTML={{ __html: node.user_picture?.id }}
+                          className="mt-6 text-xl leading-loose prose"
+                        />
+                      )}
+
+                      </p>
+                    </div>
+                  </article>
+                </div>
+              ))}
+            </div>
+</div>
+        )}
+
+
+-------------------------------------------------------------------------------------------
+
+
 
 
 
@@ -107,69 +260,18 @@ chemin : {group.path.alias}
   )
 }
 
-export async function getServerSideProps(
-  context: GetServerSidePropsContext
-): Promise<GetServerSidePropsResult<AccountPageProps>> {
-  // Check if user is authenticated.
-  const session = await getSession({ ctx: context });
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
-    };
-  }
+export async function getStaticProps(
+  context
+): Promise<GetStaticPropsResult<AccountPageProps>> {
 
-  const params = new DrupalJsonApiParams()
-      .addInclude(["uid.user_picture"])
-      .addSort("created", "ASC")
-
-
-
-const groups = await drupal.getResourceCollection<DrupalNode[]>(
-  "group_content--federage-group_membership",
+const results = await getSearchIndexFromContext<JsonApiSearchApiResponse>(
+  "default_index",
+  context,
   {
-    params: new DrupalJsonApiParams()
-      .addInclude(["uid", "gid"])
-      .addFields("group_relationship--projets_federage-b5856fc584d18c4", ["id", "group_content_type","meta"])
-
-      .addSort("created", "DESC")
-      .getQueryObject(),
-
-    withAuth: session.accessToken,
-
+    deserialize: false,
+params,
   }
-
 )
-
-const greltypes = await drupal.getResourceCollection<DrupalNode[]>(
-  "group_content_type--group_content_type",
-  {
-
-    withAuth: session.accessToken,
-
-  }
-
-)
-const members = await drupal.getResourceCollection<DrupalNode[]>(
-  "group_content--federage-group_node-financement",
-  {
-    params: new DrupalJsonApiParams()
-      .addInclude(["uid" ])
-      .addFields("group_content--federage-group_node-financement", ["id", "gid","meta"])
-
-      .addSort("created", "DESC")
-      .getQueryObject(),
-      params: {
-
-      },
-    withAuth: session.accessToken,
-
-  }
-
-)
-
 
 
 
@@ -178,9 +280,9 @@ const members = await drupal.getResourceCollection<DrupalNode[]>(
   return {
      props: {
        ...(await getGlobalElements(context)),
-       groups,
-       members,
-       greltypes,
+
+       nodes: deserialize(results) as DrupalNode[],
+       facets: results.meta.facets,
      },
    };
  }
