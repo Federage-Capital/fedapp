4e536ac7a475822300e00617d5d1a6d028fcb44c
septembre-io <contact@septembre.io>
2023-05-16 12:06:03 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Scope+One&display=swap"
-            rel="stylesheet"
-          />
+    
         </Head>
         <body>
           <Main />
