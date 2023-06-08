ad338ca691c9ca4aabf069b5e9e7a51d3bbc52be
Steven ZABANDITH <steven.zabandith@epitech.eu>
2023-06-08 11:52:04 +0200

    [ADD] in menu index the username + logout when the user is connected

--- components/menu-main.tsx
+++ components/menu-main.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router"
 import { MenuUser } from "components/menu-user"
 import Image from 'next/image';
 import * as React from "react"
-import { signOut } from "next-auth/react"
+import { useSession, signOut } from "next-auth/react"
 import { MenuDring } from "components/menu-dring"
 
 interface MenuMainProps {
@@ -15,9 +15,87 @@ interface MenuMainProps {
 export function MenuMain({ items, ...props }: MenuMainProps) {
   const router = useRouter()
   const [showMenu, setShowMenu] = React.useState<Boolean>(false)
+  const { status } = useSession()
+
+  if (status === "authenticated" && router.asPath === "/") {
+    return (
+      <nav {...props}>
+        <ul className="items-center justify-center space-y-10 rounded w-screen">
+          {items.map((item) => {
+            const isActive =
+              router.asPath === item.url ||
+              `/${router.locale}${router.asPath === "/" ? "" : router.asPath}` ===
+              item.url ||
+              (item.url !== "/" ? router.asPath.indexOf(item.url) === 0 : false)
+
+            return (
+
+              <li key={item.id} className="relative h-10 w-80">
+                <Link href={item.url} passHref>
+                  <a
+                    className={classNames(
+                      "text-lg border-b-[3px] text-gray-500 flex border-b-transparent transition-colors hover:text-primary absolute inset-y-50 left-0",
+                      {
+                        "border-b-primary": isActive,
+                      }
+                    )}
+                  >
+                    <div className="ml-5">
+                      {item.title === 'Blog' && (
+                        <div className="pr-5 -px-3 flex items-center">
+                          <Image src="/blog.svg" height={40} width={40} alt="blog" />
+                          <p className="ml-2">{item.title}</p>
+                        </div>
+                      )}
+                      {item.title === 'Explorer' && (
+                        <div className="pr-5 -px-3 flex items-center">
+                          <Image src="/explorer.svg" height={40} width={40} alt="explorer" />
+                          <p className="ml-2">{item.title}</p>
+                        </div>
+                      )}
+                      {item.title !== 'Blog' && item.title !== 'Explorer' && (
+                        <div className="pr-5 -px-3 flex items-center">
+                          <Image src="/documentation.svg" height={40} width={40} alt="documentation" />
+                          <p className="ml-2">{item.title}</p>
+                        </div>
+                      )}
+                    </div>
+                  </a>
+                </Link>
+              </li>
+
+            )
+          })}
+
+          <div className="border rounded-b-lg">
+            <div className="flex">
+              <div className="md:hidden ml-5 mt-3"><MenuUser /></div>
+              <div className="absolute inset-y-50 right-0 flex items-center pr-5 sm:static sm:inset-auto sm:ml-6 sm:pr-0 mt-5">
+                <button
+                  type="button"
+                  className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-fedblue focus:ring-offset-2"
+                >
+                  <span className="sr-only">View notifications</span>
+                  <MenuDring />
+                </button>
 
 
+              </div>
+            </div>
+            <p className="ml-5 mt-5 text-gray-500"> Profil</p>
+            <p className="ml-5 mt-5 text-gray-500">Réglage</p>
+            <button
+              className={classNames(showMenu ? 'bg-gray-100' : '', 'block ml-5 text-sm text-gray-500 mt-5 pb-5')}
 
+              onClick={() => signOut()}
+            >
+              <p>Déconnexion</p>
+            </button>
+          </div>
+        </ul>
+      </nav>
+    );
+  }
   return (
     <nav {...props}>
       <ul className="items-center justify-center space-y-10 rounded w-screen">
@@ -66,34 +144,36 @@ export function MenuMain({ items, ...props }: MenuMainProps) {
 
           )
         })}
-
-        {/* <div className="border rounded-b-lg">
-          <div className="flex"> */}
         <div className="md:hidden ml-0 mt-3"><MenuUser /></div>
-        {/* <div className="absolute inset-y-50 right-0 flex items-center pr-5 sm:static sm:inset-auto sm:ml-6 sm:pr-0 mt-5">
-              <button
-                type="button"
-                className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-fedblue focus:ring-offset-2"
-              >
-                <span className="sr-only">View notifications</span>
-                <MenuDring />
-              </button>
-
-
-            </div>
-          </div>
-          <p className="ml-5 mt-5 text-gray-500"> Profil</p>
-          <p className="ml-5 mt-5 text-gray-500">Réglage</p>
-          <button
-            className={classNames(showMenu ? 'bg-gray-100' : '', 'block ml-5 text-sm text-gray-500 mt-5 pb-5')}
-
-            onClick={() => signOut()}
-          >
-            <p>Déconnexion</p>
-          </button>
-        </div> */}
       </ul>
 
     </nav>
   )
 }
+
+
+//     <div className="border rounded-b-lg">
+//       <div className="flex">
+//         <div className="md:hidden ml-0 mt-3"><MenuUser /></div>
+//         <div className="absolute inset-y-50 right-0 flex items-center pr-5 sm:static sm:inset-auto sm:ml-6 sm:pr-0 mt-5">
+//           <button
+//             type="button"
+//             className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-fedblue focus:ring-offset-2"
+//           >
+//             <span className="sr-only">View notifications</span>
+//             <MenuDring />
+//           </button>
+
+
+//         </div>
+//       </div>
+//       <p className="ml-5 mt-5 text-gray-500"> Profil</p>
+//       <p className="ml-5 mt-5 text-gray-500">Réglage</p>
+//       <button
+//         className={classNames(showMenu ? 'bg-gray-100' : '', 'block ml-5 text-sm text-gray-500 mt-5 pb-5')}
+
+//         onClick={() => signOut()}
+//       >
+//         <p>Déconnexion</p>
+//       </button>
+//     </div>
