6074e842883884f78f53c36829d1e50644b26d87
Steven ZABANDITH <steven.zabandith@epitech.eu>
2023-06-16 14:23:04 +0200

    [ADD] Link for balise p

--- components/menu-connexion.tsx
+++ components/menu-connexion.tsx
@@ -13,12 +13,22 @@ const MenuConnexion = (props: MenuConnexionProps) => {
 	return (
 		<div className="mt-5">
 			<div className="flex space-x-12">
-				<Link href="/mentions-legales" passHref>
-					<p className="ml-2">{props.text}</p>
-				</Link>
-				<p className="px-12">{props.text_confi}</p>
+				<button>
+					<Link href="/mentions-legales" passHref>
+						<p className="ml-2">{props.text}</p>
+					</Link>
+				</button>
+				<button>
+					<Link href="/confidentialite" passHref>
+						<p className="px-12">{props.text_confi}</p>
+					</Link>
+				</button>
 			</div>
-			<p className="mt-3 ml-2 pb-5">{props.text_press}</p>
+			<button>
+				<Link href="/cgu" passHref>
+					<p className="mt-3 ml-2 pb-5">{props.text_press}</p>
+				</Link>
+			</button>
 			<div className="flex justify-center">
 				<button className="px-3 fedblue py-2 text-md text-white transition-colors rounded-xl
 					cursor-pointer bg-link hover:bg-white hover:text-whote border-link w-5/6">
@@ -30,9 +40,11 @@ const MenuConnexion = (props: MenuConnexionProps) => {
 			<div className="flex justify-center mx-auto">
 				<div className="mt-5 relative pb-0 mr-12">
 					<p className="text-gray-500 font-semibold">Jamais inscrit ?</p>
-					<Link href="/register" passHref>
-						<p className="relative -top-6 left-[125px] fedblueblue font-semibold">Cliquez ici</p>
-					</Link>
+					<button>
+						<Link href="/register" passHref>
+							<p className="relative -top-6 left-[125px] fedblueblue font-semibold">Cliquez ici</p>
+						</Link>
+					</button>
 				</div>
 			</div>
 		</div >
