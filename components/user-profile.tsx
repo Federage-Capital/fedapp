a3bac94200558b36b230fbe2aa484655fffa1f28
Steven ZABANDITH <steven.zabandith@epitech.eu>
2023-06-16 16:59:08 +0200

    [ADD] create component for user-profile

--- components/user-profile.tsx
+++ components/user-profile.tsx
@@ -3,6 +3,7 @@ import { DrupalNode } from "next-drupal"
 import useSWR from 'swr'
 
 import { absoluteURL, formatDate } from "lib/utils"
+import { UserListCard } from "components/card-user-profile"
 
 interface UserProfileProps {
   node: DrupalNode
@@ -14,30 +15,21 @@ export function UserProfile({ node, ...props }: UserProfileProps) {
 
 
 
-  const { data: userprojects, error: userprojectsError  } = useSWR(() =>'https://fed.septembre.io/user-s-projects-rest/' + node.id, fetcher)
+  const { data: userprojects, error: userprojectsError } = useSWR(() => 'https://fed.septembre.io/user-s-projects-rest/' + node.id, fetcher)
 
 
-                  if (!userprojects) return <div>Loading User's project ...</div>
+  if (!userprojects) return <div>Loading User's project ...</div>
 
   return (
     <article {...props}>
-
-
-
-
-
-
       <h1 className="mb-4 text-6xl font-black leading-tight">  {node.display_name}</h1>
-
-
       <div className="mb-4 text-gray-600">
-
-       {node.field_description?.processed && (
-         <div
-           dangerouslySetInnerHTML={{ __html: node.field_description?.processed }}
-           className="mt-6 text-xl leading-loose prose"
-         />
-       )}
+        {node.field_description?.processed && (
+          <div
+            dangerouslySetInnerHTML={{ __html: node.field_description?.processed }}
+            className="mt-6 text-xl leading-loose prose"
+          />
+        )}
       </div>
       {node.field_image && (
         <figure>
@@ -64,32 +56,18 @@ export function UserProfile({ node, ...props }: UserProfileProps) {
         />
       )}
 
-
-
-
-
-          {userprojects?.length ? (
-
-
-          <p>
-              {userprojects.map((sesprojets,index) => (
-
-      <p>
-
-
-      {sesprojets.label}
-      </p>
-
-                                    ))}
-
-
-
-          </p>
-                                                        ) : (
-
-                                                                <p>   Ajouter un financement</p>
-
-                                                        )}
+      <UserListCard key={node.id} node={node} />
+      {userprojects?.length ? (
+        <p>
+          {userprojects.map((sesprojets, index) => (
+            <p>
+              {sesprojets.label}
+            </p>
+          ))}
+        </p>
+      ) : (
+        <p>   Ajouter un financement</p>
+      )}
     </article>
   )
 }
