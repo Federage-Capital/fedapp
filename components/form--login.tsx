d45ac80eb182b317bfac8f5f88529de83d0602e9
Steven ZABANDITH <steven.zabandith@epitech.eu>
2023-06-19 16:01:13 +0200

    [FIX] space

--- components/form--login.tsx
+++ components/form--login.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "next-i18next"
 import { signIn } from "next-auth/react"
 import { useRouter } from "next/router"
 
-interface FormLoginProps extends React.HTMLProps<HTMLFormElement> {}
+interface FormLoginProps extends React.HTMLProps<HTMLFormElement> { }
 
 interface FormStatus {
   status: "success" | "error" | "fetching"
@@ -44,55 +44,65 @@ export function FormLogin({ className, ...props }: FormLoginProps) {
   }
 
   return (
-    <form
-      className={classNames("grid gap-4", className)}
-      onSubmit={onSubmit}
-      {...props}
-    >
-      {formStatus?.message && (
-        <p
-          className={classNames("py-3 px-4 border", {
-            "border-link bg-link/10 text-link":
-              formStatus?.status === "success",
-            "border-error bg-error/10 text-error":
-              formStatus?.status === "error",
-          })}
-        >
-          {formStatus.message}
-        </p>
-      )}
+    <div>
+      <form
+        className={classNames("grid gap-4", className)}
+        onSubmit={onSubmit}
+        {...props}
+      >
+        {formStatus?.message && (
+          <p
+            className={classNames("py-3 px-4 border", {
+              "border-link bg-link/10 text-link":
+                formStatus?.status === "success",
+              "border-error bg-error/10 text-error":
+                formStatus?.status === "error",
+            })}
+          >
+            {formStatus.message}
+          </p>
+        )}
 
-      <div className="grid">
+        <div className="grid">
 
-        <input
-          id="username"
-          name="username"
-          maxLength={255}
-          placeholder="Adresse mail"
-          required
-          className="relative block w-full px-3 py-2 mb-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-black focus:border-black focus:z-10 sm:text-sm"
-        />
+          <input
+            id="username"
+            name="username"
+            maxLength={255}
+            placeholder="Adresse mail"
+            required
+            className="relative block w-full px-3 py-2 mb-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-black focus:border-black focus:z-10 sm:text-sm"
+          />
 
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Mot de passe"
-          required
-          className="relative block w-full px-3 py-2 mb-4 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-black focus:border-black focus:z-10 sm:text-sm"
-        />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Mot de passe"
+            required
+            className="relative block w-full px-3 py-2 mb-4 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-black focus:border-black focus:z-10 sm:text-sm"
+          />
 
 
 
-        <input
-          type="submit"
-          className="px-3 fedblue py-2 text-md text-white w-full transition-colors rounded-xl cursor-pointer bg-link hover:bg-white hover:text-whote border-link"
-          disabled={formStatus?.status === "fetching"}
-          value={
-            formStatus?.status === "fetching" ? t("Patientez") : t("Connexion")
-          }
-        />
-      </div>
-    </form>
+          <input
+            type="submit"
+            className="px-3 fedblue py-2 text-md text-white w-full transition-colors rounded-xl cursor-pointer bg-link hover:bg-white hover:text-whote border-link"
+            disabled={formStatus?.status === "fetching"}
+            value={
+              formStatus?.status === "fetching" ? t("Patientez") : t("Connexion")
+            }
+          />
+        </div>
+        <div>
+        </div>
+        <div>
+        </div>
+        <div>
+        </div>
+        <div>
+        </div>
+      </form>
+    </div>
   )
 }
--- components/header.tsx
+++ components/header.tsx
@@ -83,7 +83,6 @@ export function Header({ menus }: HeaderProps) {
               <a className="flex justify-start">
                 <div className="ml-5">
                   <Image src="/federage-logo.svg" height={100} width={200} alt="Logo federage" />
-
                 </div>
 
                 <span className="sr-only">{siteConfig.name}</span>
