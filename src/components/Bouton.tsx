8c7d0ca3368b80b2227e7c187f46bb5a898db093
ramponneau <ramponneau>
2023-07-28 12:04:21 +0000

    Pushed from Locofy Builder

--- src/components/Bouton.tsx
+++ src/components/Bouton.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from "react";
 const Bouton: FunctionComponent = () => {
   return (
     <div className="relative rounded-lg bg-gray-100 flex flex-row py-3.5 px-4 items-start justify-start">
-      <img className="relative w-[5.7px] h-[9.5px]" alt="" src="/icon1.svg" />
+      <img className="relative w-[5.7px] h-[9.5px]" alt="" src="/icon12.svg" />
     </div>
   );
 };
--- src/components/Volet1.tsx
+++ src/components/Volet1.tsx
@@ -1,15 +1,22 @@
-import { FunctionComponent } from 'react';
-import C$209:20755$ from "F$209:20755$";
+import { FunctionComponent } from "react";
 
-const Volet1:FunctionComponent = () => {
-
+const Volet1: FunctionComponent = () => {
   return (
-    <div className="relative rounded-8xs box-border w-[366.5px] h-[91px] overflow-hidden text-left text-sm text-mediumblue-100 font-text-2xl-leading-8-font-bold border-[1px] border-dashed border-blueviolet"><Property1VoletInactif />
+    <div className="relative rounded-8xs box-border w-[366.5px] h-[91px] overflow-hidden text-left text-sm text-mediumblue-100 font-text-2xl-leading-8-font-bold border-[1px] border-dashed border-blueviolet">
+      <div className="absolute top-[20px] left-[20px] flex flex-row items-center justify-center gap-[8px]">
+        <div className="relative leading-[20.03px] font-semibold">
+          Détails de l’apport
+        </div>
+        <img className="relative w-2.5 h-1.5" alt="" src="/icon311.svg" />
+      </div>
       <div className="absolute top-[50px] left-[20.5px] w-[326px] flex flex-row items-center justify-start gap-[8px]">
-        <div className="relative leading-[20.03px] font-semibold">Détails de l’apport</div>
+        <div className="relative leading-[20.03px] font-semibold">
+          Détails de l’apport
+        </div>
         <img className="relative w-2.5 h-1.5" alt="" src="/icon1111.svg" />
       </div>
-    </div>);
+    </div>
+  );
 };
 
 export default Volet1;
