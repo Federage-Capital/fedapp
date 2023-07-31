60aaf3dac53826ecd81e1bde28171572a7f16b90
Steven ZABANDITH <steven.zabandith@epitech.eu>
2023-07-31 17:00:46 +0200

    [FIX] move a div inside the map

--- components/card-slide_apport.tsx
+++ components/card-slide_apport.tsx
@@ -15,7 +15,6 @@ export default function SlideApport({ showMenu, node }: SlideApportAllUserListPr
 
 	return (
 		<>
-			{/* {userApport} */}
 			{showMenu && (
 				<div className="self-stretch flex flex-col items-center justify-start text-black1 pb-5 ">
 					<div className="self-stretch rounded-t-lg rounded-b-none flex flex-row p-4 items-center justify-between border-[2px] border-solid border-gray-100">
@@ -32,14 +31,14 @@ export default function SlideApport({ showMenu, node }: SlideApportAllUserListPr
 											.map((userInApport) => (
 												<div key={userInApport.id}>
 													{userInApport}
+													<div className="self-stretch relative text-xs leading-[24px] font-semibold text-mediumblue-100">
+														Prix recherché : 40 000,00€
+													</div>
 												</div>
 											))
 										}
 									</div>
 								)}
-							</div>
-							<div className="self-stretch relative text-xs leading-[24px] font-semibold text-mediumblue-100">
-								Prix recherché : 40 000,00€
 							</div>
 						</div>
 						<Link href={`/group/federage/${node.label.replace(/è/g, 'e').replaceAll(' ', '-')}`} passHref>
@@ -51,7 +50,7 @@ export default function SlideApport({ showMenu, node }: SlideApportAllUserListPr
 							</button>
 						</Link>
 					</div>
-					<div className="self-stretch rounded-t-none rounded-b-lg flex flex-row p-4 items-center justify-between border-r-[2px] border-solid border-gray-100 border-b-[2px] border-l-[2px]">
+					{/* <div className="self-stretch rounded-t-none rounded-b-lg flex flex-row p-4 items-center justify-between border-r-[2px] border-solid border-gray-100 border-b-[2px] border-l-[2px]">
 						<div className="flex-1 flex flex-col py-0 pr-2.5 pl-0 items-start justify-start gap-[2px]">
 							<div className="self-stretch relative leading-[20px] font-semibold">
 								Laboratoire de type Cancéropôle
@@ -64,7 +63,7 @@ export default function SlideApport({ showMenu, node }: SlideApportAllUserListPr
 							<rect x="0.797363" width="38.2027" height="38" rx="8.10667" fill="#F3F4F6" />
 							<path fill-rule="evenodd" clip-rule="evenodd" d="M17.2927 23.5314C16.9168 23.1555 16.9168 22.5459 17.2927 22.17L20.4627 19L17.2927 15.8301C16.9168 15.4541 16.9168 14.8446 17.2927 14.4687C17.6686 14.0927 18.2782 14.0927 18.6541 14.4687L22.5048 18.3193C22.8807 18.6953 22.8807 19.3048 22.5048 19.6807L18.6541 23.5314C18.2782 23.9073 17.6686 23.9073 17.2927 23.5314Z" fill="#52606D" />
 						</svg>
-					</div>
+					</div> */}
 				</div >
 			)
 			}
