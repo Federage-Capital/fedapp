1c5a25ce1e492a7f2317735d083c758c2c836366
septembre-io <contact@septembre.io>
2023-09-29 19:27:40 +0200

    node-financement unauthenticate

--- components/node--financement.tsx
+++ components/node--financement.tsx
@@ -19,7 +19,13 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export function NodeFinancement({ node, ...props }: NodeFinancementProps) {
   const router = useRouter()
-  const session = getSession()
+  const { data: session, status } = useSession()
+
+  const [showMenu, setShowMenu] = React.useState<boolean>(false)
+
+	const changeState = () => {
+		setShowMenu(!showMenu);
+	}
 
 
   const { t } = useTranslation()
@@ -41,23 +47,61 @@ export function NodeFinancement({ node, ...props }: NodeFinancementProps) {
 
 
 
-                                               <p
-                                                 dangerouslySetInnerHTML={{ __html: node.title }}
-                                                 className="text-3xl font-bold"
-                                               />
 
-  ===  {node.body?.value} ===
-Publié par :
-<span
-  dangerouslySetInnerHTML={{ __html: node.uid.display_name }}
-  className="pl-1 text-lg font-semibold fedblueblue"
-/>
+      <div
+        dangerouslySetInnerHTML={{ __html: node.title }}
+        className="text-2xl font-bold"
+      />
+
+      Publié par :   <span
+        dangerouslySetInnerHTML={{ __html: node.uid.display_name }}
+        className="pl-1 text-lg font-semibold fedblueblue"
+      />
+
+<div className="card shadow-md rounded-md p-5 text-center mb-12">
+      {showMenu ? (
+      <>	Afficher plus		<span className="inline-block ml-2" onClick={changeState}>
+          <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M13.5899 8.78996C13.0432 9.3367 12.1568 9.3367 11.6101 8.78996L7 4.1799L2.38995 8.78996C1.84322 9.3367 0.956784 9.3367 0.410051 8.78996C-0.136684 8.24323 -0.136684 7.35679 0.410051 6.81006L6.01005 1.21005C6.55678 0.663316 7.44322 0.663316 7.98995 1.21005L13.5899 6.81006C14.1367 7.35679 14.1367 8.24323 13.5899 8.78996Z" fill="#012BDD" />
+          </svg>
+        </span>
+        </>
+      ) : (
+           <>	Afficher plus	<span className="inline-block ml-2" onClick={changeState}>
+          <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M0.410051 1.2101C0.956784 0.663365 1.84322 0.663365 2.38995 1.2101L7 5.82016L11.6101 1.2101C12.1568 0.663365 13.0432 0.663365 13.5899 1.2101C14.1367 1.75683 14.1367 2.64327 13.5899 3.19L7.98995 8.79001C7.44322 9.33675 6.55678 9.33675 6.01005 8.79001L0.410051 3.19C-0.136683 2.64327 -0.136683 1.75683 0.410051 1.2101Z" fill="#012BDD" />
+          </svg>
+        </span>
+        </>
+      )}
 
 
-<div className="card shadow-md p-5">
+          {showMenu && (
+            <>
+            {node.body?.processed && (
+              <div
+                dangerouslySetInnerHTML={{ __html: node.body?.value }}
+                className="text-2xl text-left font-bold"
+              />
+            )}
+              </>
+            )
+            }
+</div>
+
+
+
+<div className="card shadow-md rounded-md p-5  mb-12">
 
 <div className="grid grid-cols-12 gap-4 pb-10">
-  <div className="col-span-10 text-xl font-bold">Détail de l&apos;apport</div>
+
+  <div className="col-span-10 text-lg font-semibold text-left">
+
+Statut de l’apport
+
+</div>
+
+
   <div className="col-span-2 text-right">
 
 
@@ -73,19 +117,45 @@ Publié par :
 </div>
 
 
-<span className="text-base ">Prix négocié</span>
-<span className="text-base font-medium text-slate-500 pb-5">{node.field_estimation_du_prix}€</span>
+<p className="text-base ">Prix proposé</p>
+<p className="text-base font-medium text-slate-500 pb-5">{node.field_estimation_du_prix}€</p>
 
-<span className="text-base ">Délai de livraison</span>
-<span className="text-base font-medium text-slate-500 pb-5">{formatDate(node.field_date_de_livraison)}</span>
+<p className="text-base ">Délai de livraison</p>
+<p className="text-base font-medium text-slate-500 pb-5">{formatDate(node.field_date_de_livraison)}</p>
 
-<a
-  href={`/financement/edit?gid=${encodeURIComponent(node.id)}`}
+{status === "authenticated" ? (
+         <>
+         <div className="self-stretch rounded-lg bg-indigo-600 text-white shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)] h-[47px] overflow-hidden shrink-0 flex flex-row py-2.5 px-[18px] box-border items-center justify-center text-dimgray">
+          <div className="relative leading-[20px] font-semibold">
+         <a
+           href={`/financement/edit?gid=${encodeURIComponent(node.id)}`}
 
->
+         >
 
-  <button className="fedblue text-white w-full rounded p-1">Modifier</button>
-</a>
+           Modifier
+         </a>
+         </div>
+         </div>
+         </>
+       ) : (
+
+
+
+         <div className="self-stretch rounded-lg bg-zinc-200  shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)] h-[47px] overflow-hidden shrink-0 flex flex-row py-2.5 px-[18px] box-border items-center justify-center text-dimgray">
+          <div className="relative leading-[20px] font-semibold">
+          <a
+            href={`/financement/edit?gid=${encodeURIComponent(node.id)}`}
+
+          >
+
+        Modifier
+          </a>
+          </div>
+        </div>
+       )}
+
+
+
 </div>
 <div className="card shadow-md p-5">
 
